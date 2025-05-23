c858449cee4942d98a28c89ae1811d5e479c34cc
MColomba <matiascolomba@gmail.com>
2025-05-23 19:03:57 -0300

    se crea la rama para Auditoria y se crea el formulario con sus componentes

---
+++ frmAuditoria.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace pryColomba_IEFI
+{
+    partial class frmAuditoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAuditoria = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuUsuario = new System.Windows.Forms.ToolStripMenuItem();
+            this.detalleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resumenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAuditoria)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvAuditoria
+            // 
+            this.dgvAuditoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAuditoria.Location = new System.Drawing.Point(13, 73);
+            this.dgvAuditoria.Name = "dgvAuditoria";
+            this.dgvAuditoria.Size = new System.Drawing.Size(240, 150);
+            this.dgvAuditoria.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuUsuario});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuUsuario
+            // 
+            this.mnuUsuario.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detalleToolStripMenuItem,
+            this.resumenToolStripMenuItem});
+            this.mnuUsuario.Name = "mnuUsuario";
+            this.mnuUsuario.Size = new System.Drawing.Size(59, 20);
+            this.mnuUsuario.Text = "Usuario";
+            // 
+            // detalleToolStripMenuItem
+            // 
+            this.detalleToolStripMenuItem.Name = "detalleToolStripMenuItem";
+            this.detalleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.detalleToolStripMenuItem.Text = "Detalle";
+            // 
+            // resumenToolStripMenuItem
+            // 
+            this.resumenToolStripMenuItem.Name = "resumenToolStripMenuItem";
+            this.resumenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resumenToolStripMenuItem.Text = "Resumen";
+            // 
+            // frmAuditoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvAuditoria);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAuditoria";
+            this.Text = "Auditoria";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAuditoria)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAuditoria;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuUsuario;
+        private System.Windows.Forms.ToolStripMenuItem detalleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resumenToolStripMenuItem;
+    }
+}
